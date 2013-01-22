6d6e478c647fe7a19b47573687e1c268406a0e2e
alex <argriffi@ncsu.edu>
2013-01-22 12:38:32 -0500

    simplify the maintenance setup script

--- maintenance-setup.py
+++ maintenance-setup.py
@@ -5,16 +5,12 @@ $ python maintenance-setup.py build_ext --inplace
 """
 
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 my_ext_name = 'pyfelscore'
-
-my_ext = Extension(my_ext_name, [my_ext_name + '.pyx'])
 
 setup(
         name = my_ext_name,
         version = '0.1',
-        cmdclass = { 'build_ext' : build_ext },
-        ext_modules = [my_ext],
+        ext_modules = cythonize(my_ext_name + '.pyx')
         )
