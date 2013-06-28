cc7418346bd97dca8c6e2ddfba800623b2695a3a
alex <argriffi@ncsu.edu>
2013-06-27 21:26:42 -0400

    ENH: push more stuff into cython

--- pyfelscore.pyx
+++ pyfelscore.pyx
@@ -28,6 +28,9 @@ __all__ = [
         'get_mmpp_frechet_diagonalizable_w_zero',
         'mcy_get_node_to_pset',
         'get_node_to_set',
+        'mcy_esd_get_node_to_pset',
+        'esd_get_node_to_set',
+        'mcy_esd_get_node_to_pmap',
         ]
 
 
@@ -272,10 +275,175 @@ def align_rooted_star_tree(
 
 ###############################################################################
 # More recent attempts at speedups related to likelihood functions.
+#
 # Terminology like 'csr', 'csc', 'indices', and 'indptr' is intended
 # to be compatible with the corresponding sparse matrix jargon.
 # For example, 'csr' is 'compressed sparse rows',
 # and 'csc' is 'comparessed sparse columns'.
+#
+# The 'mcy' means 'markov chain with observation type y' where 'type y'
+# means that for each node of the tree,
+# a set of set of states is possible.
+# This is observation type is more general than allowing the state
+# to be only either known or unknown at each node,
+# but it is less general than a full hidden-Markov framework
+# for which each state at each node is associated with some
+# emission probability.
+#
+# The 'esd' terminology means 'edge specific dense transition matrix'.
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def mcy_esd_get_node_to_pmap(
+        np.int_t [:] tree_csr_indices,
+        np.int_t [:] tree_csr_indptr,
+        #
+        np.float_t[:, :, :] esd_transitions, # (nnodes, nstates, nstates)
+        #
+        np.int_t [:, :] state_mask, # (nnodes, nstates)
+        np.float_t[:, :] subtree_probability, # (nnodes, nstates)
+        ):
+    """
+    Map each node to the set of states for which a subtree is feasible.
+
+    The first group of args defines the structure of the rooted tree.
+    Its nodes should be indexed in preorder from the root,
+    so node 0 is the root and the rest of the nodes are ordered
+    according to a preorder traversal.
+
+    The second group of args is a single arg that defines the edge-specific
+    dense transition matrices.
+    For a rooted tree, the non-root nodes are in a bijective
+    correspondence to the edges of the tree.
+    For notational purposes the root node
+    (necessarily node 0 because the nodes are sorted in preorder from the root)
+    will also be associated with an edge,
+    but the transition matrix on this edge will be ignored.
+
+    The third arg group has information per state per node.
+    An entry in the state_mask is 0 to represent a state that is impossible
+    for whatever reason.
+    This state_mask is an input ndarray which will not be modified.
+    The subtree_probability ndarray is for output only.
+    An entry in subtree_probability will be 0 if the corresponding
+    entry in the state_mask is 0, otherwise it will contain
+    the subtree probability given the state at the node.
+
+    """
+    cdef int nnodes = state_mask.shape[0]
+    cdef int nstates = state_mask.shape[1]
+    cdef int node_ind_start, node_ind_stop
+    cdef double multiplicative_prob
+    cdef double additive_prob
+    cdef int na, nb
+    cdef int i, j
+    for i in range(nnodes):
+
+        # Define the current node.
+        na = (nnodes - 1) - i
+        node_ind_start = tree_csr_indptr[na]
+        node_ind_stop = tree_csr_indptr[na+1]
+
+        # Compute the subtree probability for each possible state.
+        for sa in range(nstates):
+            subtree_probability[na, sa] = 0
+            if not state_mask[na, sa]:
+                continue
+            multiplicative_prob = 1
+            for j in range(node_ind_start, node_ind_stop):
+                nb = tree_csr_indices[j]
+                additive_prob = 0 
+                for sb in range(nstates):
+                    if state_mask[nb, sb]:
+                        additive_prob += (
+                                esd_transitions[nb, sa, sb] *
+                                subtree_probability[nb, sb])
+                multiplicative_prob *= additive_prob
+            subtree_probability[na, sa] = multiplicative_prob
+
+    return 0
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def mcy_esd_get_node_to_pset(
+        np.int_t [:] tree_csr_indices,
+        np.int_t [:] tree_csr_indptr,
+        #
+        np.float_t[:, :, :] esd_transitions, # (nnodes, nstates, nstates)
+        #
+        np.int_t [:, :] state_mask,
+        ):
+    """
+    Map each node to the set of states for which a subtree is feasible.
+
+    The first group of args defines the structure of the rooted tree.
+    Its nodes should be indexed in preorder from the root,
+    so node 0 is the root and the rest of the nodes are ordered
+    according to a preorder traversal.
+
+    The second group of args is a single arg that defines the edge-specific
+    dense transition matrices.
+    For a rooted tree, the non-root nodes are in a bijective
+    correspondence to the edges of the tree.
+    For notational purposes the root node
+    (necessarily node 0 because the nodes are sorted in preorder from the root)
+    will also be associated with an edge,
+    but the transition matrix on this edge will be ignored.
+
+    The third arg group comprises the single dense mutable binary ndarray
+    which defines the set of allowed states for each node.
+    This ndarray will be used for both input and output;
+    as input, it will contain some zeros and some ones,
+    and upon termination of this function, some of the ones will possibly
+    have been changed to zeros.
+
+    """
+    cdef int nnodes = state_mask.shape[0]
+    cdef int nstates = state_mask.shape[1]
+    cdef int node_ind_start, node_ind_stop
+    cdef int na, nb
+    cdef int good_state_flag
+    cdef int bad_node_flag
+    cdef int i, j
+    for i in range(nnodes):
+
+        # Define the current node.
+        na = (nnodes - 1) - i
+        node_ind_start = tree_csr_indptr[na]
+        node_ind_stop = tree_csr_indptr[na+1]
+
+        # Query each potentially allowed state of the current node
+        # by checking whether for each child node,
+        # a valid state of the child node can be reached
+        # from the putative state of the current node.
+        for sa in range(nstates):
+            if not state_mask[na, sa]:
+                continue
+            bad_node_flag = 0
+            for j in range(node_ind_start, node_ind_stop):
+                nb = tree_csr_indices[j]
+                good_state_flag = 0
+                for sb in range(nstates):
+                    if esd_transitions[nb, sa, sb] and state_mask[nb, sb]:
+                        good_state_flag = 1
+                        break
+                if not good_state_flag:
+                    bad_node_flag = 1
+                    break
+
+            # If, for one of the child nodes nb,
+            # no allowed state can be reached from the
+            # current state sa of the current node na,
+            # then mark the state sa as forbidden in the state mask of na.
+            if bad_node_flag:
+                state_mask[na, sa] = 0
+
+    return 0
+
 
 
 @cython.boundscheck(False)
@@ -358,6 +526,77 @@ def mcy_get_node_to_pset(
                 state_mask[na, sa] = 0
 
     return 0
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def esd_get_node_to_set(
+        np.int_t [:] tree_csr_indices,
+        np.int_t [:] tree_csr_indptr,
+        #
+        np.float_t[:, :, :] esd_transitions, # (nnodes, nstates, nstates)
+        #
+        np.int_t [:, :] state_mask, # (nnodes, nstates)
+        ):
+    """
+    This function should be called after calling mcy_esd_get_node_to_pset.
+
+    Whereas mcy_get_node_to_pset is a backward pass,
+    from the leaves to the root,
+    the function get_node_to_set is a forward pass
+    from the root to the leaves.
+    Both of these functions use only the sparsity structure
+    of the tree and the transition matrix and the node state constraints.
+    In other words, they only care about whether or not a given event
+    is possible, rather than caring about whether the event has
+    a high vs. a low probability.
+
+    For each pre-order parent node,
+    constrain the set of allowed states of each child node.
+    Do this by constructing the set of child node states
+    which can be reached by starting in one of the allowed parent states
+    and transitioning to the child state.
+    This constructed set will act as a mask which can be applied
+    to all child node state masks.
+
+    """
+    cdef int nnodes = state_mask.shape[0]
+    cdef int nstates = state_mask.shape[1]
+    cdef int na, nb
+    cdef int sa, sb
+    cdef int node_ind_start, node_ind_stop
+    cdef int i
+    cdef int reachable_flag
+
+    # For each parent node, restrict the set of possible child node states.
+    for na in range(nnodes):
+
+        # Get the info about the child nodes implied by the tree shape.
+        node_ind_start = tree_csr_indptr[na]
+        node_ind_stop = tree_csr_indptr[na+1]
+
+        # If there are no child nodes then skip.
+        if node_ind_start == node_ind_stop:
+            continue
+
+        # For each child node,
+        # restrict the set of allowed states,
+        # by allowing only the states which can be reached in one step
+        # from a state that is allowed at the parent node.
+        for i in range(node_ind_start, node_ind_stop):
+            nb = tree_csr_indices[i]
+            for sb in range(nstates):
+                reachable_flag = 0
+                for sa in range(nstates):
+                    if state_mask[na, sa] and esd_transitions[nb, sa, sb]:
+                        reachable_flag = 1
+                        break
+                if not reachable_flag:
+                    state_mask[nb, sb] = 0
+
+    return 0
+
 
 
 @cython.boundscheck(False)
